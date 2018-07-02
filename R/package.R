c23c7295255b5ae1d5c9fd22669b9d85bde95fca
Robrecht Cannoodt <brechtus@gmail.com>
2018-07-02 15:28:57 +0200

    add package documentation

---
+++ R/package.R
@@ -0,0 +1,10 @@
+#' Common dimensionality reduction methods
+#'
+#' Provides a common interface for applying common dimensionality reduction methods,
+#' Such as PCA, ICA, diffusion maps, LLE, t-SNE, and umap.
+#'
+#' @importFrom dynutils correlation_distance install_packages
+#'
+#' @docType package
+#' @name dyndimred
+NULL
