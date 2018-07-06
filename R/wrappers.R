dcc9ee93b496f4b7cc77422f2c6f909232ac3468
zouter <wouter.saelens@gmail.com>
2018-07-06 11:21:40 +0200

    add methods to list_dimred_methods

--- R/wrappers.R
+++ R/wrappers.R
@@ -7,10 +7,12 @@ list_dimred_methods <- function() {
     tsne = dimred_tsne,
     ica = dimred_ica,
     lle = dimred_lle,
+    landmark_mds = dimred_landmark_mds,
     mds_sammon = dimred_mds_sammon,
     mds_isomds = dimred_mds_isomds,
     mds_smacof = dimred_mds_smacof,
-    umap = dimred_umap
+    umap = dimred_umap,
+    dm_diffusionMap = dimred_dm_diffusionMap
   )
 }
 
