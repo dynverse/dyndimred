be9d4960bc92d567a6a2f9e47d3327b5b6b1f117
Robrecht Cannoodt <rcannood@gmail.com>
2019-02-15 16:52:32 +0100

    move process_dimred function

--- R/dimred_umap.R
+++ R/dimred_umap.R
@@ -10,9 +10,3 @@ dimred_umap <- function(x, ndim = 2, n_neighbors = 15L, alpha = 1, init = "spect
   space <- uwot::umap(x, n_components = ndim, n_neighbors = n_neighbors, alpha = alpha, init = init, n_threads = n_threads)
   process_dimred(space, rownames(x))
 }
-
-process_dimred <- function(space, rn = rownames(space)) {
-  space <- as.matrix(space)
-  dimnames(space) <- list(rn, paste0("comp_", seq_len(ncol(space))))
-  space
-}
