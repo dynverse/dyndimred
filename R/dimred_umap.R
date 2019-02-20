46427b436a153b5a7325d52d6f1717c232e2a1f9
Robrecht Cannoodt <rcannood@gmail.com>
2019-02-20 15:15:13 +0100

    uwot::umap does not the 'alpha' parameter anymore

--- R/dimred_umap.R
+++ R/dimred_umap.R
@@ -3,10 +3,10 @@
 #' @inheritParams uwot::umap
 #' @seealso [uwot::umap()]
 #' @export
-dimred_umap <- function(x, ndim = 2, n_neighbors = 15L, alpha = 1, init = "spectral", n_threads = 1) {
+dimred_umap <- function(x, ndim = 2, n_neighbors = 15L, init = "spectral", n_threads = 1) {
   dynutils::install_packages(dependencies = "uwot", package = "dyndimred")
 
   requireNamespace("uwot")
-  space <- uwot::umap(x, n_components = ndim, n_neighbors = n_neighbors, alpha = alpha, init = init, n_threads = n_threads)
+  space <- uwot::umap(x, n_components = ndim, n_neighbors = n_neighbors, init = init, n_threads = n_threads)
   process_dimred(space, rownames(x))
 }
