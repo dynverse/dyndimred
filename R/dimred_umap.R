16af99a2724156a3025c19ba937d3a03414153d7
Wouter Saelens <wouter.saelens@gmail.com>
2019-03-29 05:59:04 +0100

    use uwot

--- R/dimred_umap.R
+++ R/dimred_umap.R
@@ -3,19 +3,14 @@
 #' @inheritParams uwot::umap
 #' @seealso [uwot::umap()]
 #' @export
-dimred_umap <- function(x, ndim = 2, n_neighbors = 15L, init = "spectral") {
-  required_check("umap")
+dimred_umap <- function(x, ndim = 2, n_neighbors = 15L, init = "spectral", n_threads = 1) {
+  required_check("uwot")
 
   if (dynutils::is_sparse(x)) {
     x <- as.matrix(x)
   }
 
-  config <- umap::umap.defaults
-  config$n_components <- ndim
-  config$n_neighbors <- n_neighbors
-  config$init <- init
-
-  requireNamespace("umap")
-  space <- umap::umap(x, config)
-  process_dimred(space$layout, rownames(x))
+  requireNamespace("uwot")
+  space <- uwot::umap(x, n_components = ndim, n_neighbors = n_neighbors, init = init, n_threads = n_threads)
+  process_dimred(space, rownames(x))
 }
