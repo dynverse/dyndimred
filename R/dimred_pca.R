ccef8aa916e40fa3169c3612c80847dd0c5ad1d1
Wouter Saelens <wouter.saelens@gmail.com>
2019-04-04 16:44:47 +0200

    clean up after robrecht

--- R/dimred_pca.R
+++ R/dimred_pca.R
@@ -1,7 +1,9 @@
 #' @rdname dimred
-#' @importFrom irlba prcomp_irlba
 #' @export
 dimred_pca <- function(x, ndim = 2) {
+  required_check("irlba")
+  requireNamespace("irlba")
+
   space <- irlba::prcomp_irlba(x, n = ndim)$x
 
   process_dimred(space, rn = rownames(x))
