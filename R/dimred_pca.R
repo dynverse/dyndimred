ca59b50cdbd7687b3b57a9804979862778069a51
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-04 13:40:23 +0200

    use irlba prcomp instead of stats prcomp

--- R/dimred_pca.R
+++ R/dimred_pca.R
@@ -1,6 +1,8 @@
 #' @rdname dimred
+#' @importFrom irlba prcomp_irlba
 #' @export
 dimred_pca <- function(x, ndim = 2) {
-  space <- stats::prcomp(x)$x[,seq_len(ndim)]
+  space <- irlba::prcomp_irlba(x, n = ndim)$x
+
   process_dimred(space)
 }
