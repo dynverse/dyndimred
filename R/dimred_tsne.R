108541c62c3481f537b077719f7f2ebaece57da4
Robrecht Cannoodt <rcannood@gmail.com>
2019-08-07 10:26:11 +0200

    add matrix to examples

--- R/dimred_tsne.R
+++ R/dimred_tsne.R
@@ -5,6 +5,7 @@
 #' @export
 #'
 #' @examples
+#' library(Matrix)
 #' dataset <- abs(Matrix::rsparsematrix(100, 100, .5))
 #' dimred_tsne(dataset, ndim = 3)
 dimred_tsne <- function(x, ndim = 2, perplexity = 30, theta = 0.5, initial_dims = 50, distance_method) {
