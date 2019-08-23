0251dc1a1e81f00c2d57569288ebcbb8739a4d05
Wouter Saelens <wouter.saelens@gmail.com>
2019-08-23 16:12:25 +0200

    add faster pca in comments

--- R/dimred_pca.R
+++ R/dimred_pca.R
@@ -4,5 +4,10 @@
 dimred_pca <- function(x, ndim = 2) {
   space <- irlba::prcomp_irlba(x, n = ndim)$x
 
+  # pca <- irlba::irlba(x, nv = ndim)
+  # space <- pca$u
+  # rownames(pca) <- rownames(x)
+  # colnames(pca) <- paste0("comp_", seq_len(ncol(pca)))
+
   .process_dimred(space, rn = rownames(x))
 }
