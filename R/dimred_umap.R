568031716443da9d6147566573641090849d7f0c
Robrecht Cannoodt <rcannood@gmail.com>
2019-09-10 11:06:00 +0200

    add distance methods for umap

--- R/dimred_umap.R
+++ R/dimred_umap.R
@@ -1,7 +1,7 @@
 #' @rdname dimred
 #' @export
 #' @inheritParams uwot::umap
-dimred_umap <- function(x, ndim = 2, n_neighbors = 15L, init = "spectral", n_threads = 1) {
+dimred_umap <- function(x, ndim = 2, distance_method = c("euclidean", "cosine", "manhattan"), n_neighbors = 15L, init = "spectral", n_threads = 1) {
   # `install_packages()` checks whether the required package is installed
   # and will prompt the user about whether it should be installed
   install_packages("uwot")
@@ -10,8 +10,16 @@ dimred_umap <- function(x, ndim = 2, n_neighbors = 15L, init = "spectral", n_thr
   if (is_sparse(x)) {
     x <- as.matrix(x)
   }
-
-  space <- uwot::umap(x, n_components = ndim, n_neighbors = n_neighbors, init = init, n_threads = n_threads)
+  distance_method <- match.arg(distance_method)
+  space <- uwot::umap(
+    x,
+    n_components = ndim,
+    n_neighbors = n_neighbors,
+    metric = distance_method,
+    init = init,
+    n_threads = n_threads,
+    nn_method = "annoy"
+  )
 
   .process_dimred(space, rownames(x))
 }
