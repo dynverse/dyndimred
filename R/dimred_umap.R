d0212a9929550fb59b2ba4ebb894885e3420075b
Wouter Saelens <wouter.saelens@gmail.com>
2019-10-02 14:27:56 +0200

    fix pca_components parameter

--- R/dimred_umap.R
+++ R/dimred_umap.R
@@ -9,7 +9,7 @@
 #' library(Matrix)
 #' dataset <- abs(Matrix::rsparsematrix(100, 100, .5))
 #' dimred_umap(dataset, ndim = 3)
-dimred_umap <- function(x, ndim = 2, distance_method = c("euclidean", "cosine", "manhattan"), n_neighbors = 15L, init = "spectral", n_threads = 1) {
+dimred_umap <- function(x, ndim = 2, distance_method = c("euclidean", "cosine", "manhattan"), pca_components = 50, n_neighbors = 15L, init = "spectral", n_threads = 1) {
   # `install_packages()` checks whether the required package is installed
   # and will prompt the user about whether it should be installed
   install_packages("uwot")
