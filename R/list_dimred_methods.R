5ebba2ce16ace3a1098b87193cfe5f3861920a3c
Robrecht Cannoodt <rcannood@gmail.com>
2021-03-22 10:44:46 +0100

    add table to dimred methods

--- R/list_dimred_methods.R
+++ R/list_dimred_methods.R
@@ -1,18 +1,24 @@
+#' @importFrom tibble tribble
+dr_methods <- tibble::tribble(
+  ~name, ~fun, ~requires,
+  # "dm_destiny", dimred_dm_destiny, "destiny",
+  "dm_diffusionMap", dimred_dm_diffusionmap, "diffusionMap",
+  "ica", dimred_ica, "fastICA",
+  "knn_fr", dimred_knn_fr, c("igraph", "RANN"),
+  "landmark_mds", dimred_landmark_mds, "lmds",
+  "lle", dimred_lle, "lle",
+  "mds_isomds", dimred_mds_isomds, "MASS",
+  "mds_sammon", dimred_mds_sammon, "MASS",
+  "mds_smacof", dimred_mds_smacof, "smacof",
+  "pca", dimred_pca, "irlba",
+  "tsne", dimred_tsne, "Rtsne",
+  "umap", dimred_umap, "uwot"
+
+)
+
 #' @rdname dimred
+#' @importFrom stats setNames
 #' @export
 list_dimred_methods <- function() {
-  list(
-    pca = dimred_pca,
-    mds = dimred_mds,
-    tsne = dimred_tsne,
-    ica = dimred_ica,
-    lle = dimred_lle,
-    landmark_mds = dimred_landmark_mds,
-    mds_sammon = dimred_mds_sammon,
-    mds_isomds = dimred_mds_isomds,
-    mds_smacof = dimred_mds_smacof,
-    umap = dimred_umap,
-    dm_diffusionmap = dimred_dm_diffusionmap,
-    dm_destiny = dimred_dm_destiny
-  )
+  stats::setNames(dr_methods$fun, dr_methods$name)
 }
