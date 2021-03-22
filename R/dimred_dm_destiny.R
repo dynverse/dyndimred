f911ad4e17d059de4c1d8ce6c5ca048d57ee1471
Robrecht Cannoodt <rcannood@gmail.com>
2021-03-22 10:42:16 +0100

    remove destiny (for now?) since it's not on bioconductor anymore

--- R/dimred_dm_destiny.R
+++ R/dimred_dm_destiny.R
@@ -1,24 +1,20 @@
-#' @rdname dimred
-#' @export
-dimred_dm_destiny <- function(x, ndim = 2, distance_method = c("euclidean", "spearman", "cosine")) {
-  # `install_packages()` checks whether the required package is installed.
-  # If the session is interactive and the package is not installed,
-  # The user will be prompted about whether it should be installed.
-  install_packages("destiny")
-  requireNamespace("destiny")
-
-  if (is_sparse(x)) {
-    x <- as.matrix(x)
-  }
-
-  distance_method<- match.arg(distance_method)
-  distance <- c(euclidean = "euclidean", cosine = "cosine", spearman = "rankcor")[distance_method]
-
-  dm <- destiny::DiffusionMap(
-    data = x,
-    n_eigs = ndim,
-    distance = distance
-  )
-
-  .process_dimred(dm@eigenvectors[,seq_len(ndim)], rownames(x))
-}
+#' #' @rdname dimred
+#' #' @export
+#' dimred_dm_destiny <- function(x, ndim = 2, distance_method = c("euclidean", "spearman", "cosine")) {
+#'   requireNamespace("destiny")
+#'
+#'   if (is_sparse(x)) {
+#'     x <- as.matrix(x)
+#'   }
+#'
+#'   distance_method<- match.arg(distance_method)
+#'   distance <- c(euclidean = "euclidean", cosine = "cosine", spearman = "rankcor")[distance_method]
+#'
+#'   dm <- destiny::DiffusionMap(
+#'     data = x,
+#'     n_eigs = ndim,
+#'     distance = distance
+#'   )
+#'
+#'   .process_dimred(dm@eigenvectors[,seq_len(ndim)], rownames(x))
+#' }
